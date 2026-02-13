9263e67be64157fb3dd8003b7759f0ea1f685e72
Vinícius Tiago de Santana <168845529+EnterName23@users.noreply.github.com>
2026-02-13 16:40:58 -0300

    Create Shall_we_continue.py
    Let's create a program along the lines of the example above.
    This program should print out the message "hi" and then ask "Shall we continue?" until the user inputs "no". 
    Then the program should print out "okay then" and finish.

---
+++ Shall_we_continue.py
@@ -0,0 +1,14 @@
+"""
+Let's create a program along the lines of the example above.
+This program should print out the message "hi" and then ask "Shall we continue?" until the user inputs "no". 
+Then the program should print out "okay then" and finish.
+"""
+
+while True:
+    print("hi")
+    response = input("Shall we continue?")
+
+    if response == "no":
+        print("okay then")
+        break
+
