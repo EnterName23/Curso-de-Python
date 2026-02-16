0478551c8123e8bdbb3c89cb5fc8403a6aacd57f
Vinícius Tiago de Santana <168845529+EnterName23@users.noreply.github.com>
2026-02-16 17:54:52 -0300

    Create Numbers.py
    Please write a program which asks the user for a number. The program then prints out all integer numbers greater than zero but smaller than the input.
    Sample output
    Upper limit: 5
    1
    2
    3
    4
    Please don't use the value True as the condition of your while loop in this exercise!

---
+++ Numbers.py
@@ -0,0 +1,16 @@
+"""
+Please write a program which asks the user for a number. The program then prints out all integer numbers greater than zero but smaller than the input.
+Sample output
+Upper limit: 5
+1
+2
+3
+4
+Please don't use the value True as the condition of your while loop in this exercise!
+"""
+upper_limit = int(input(("Upper limit: "))
+number = 1
+while number < upper_limit:
+    print(number)
+        number += 1
+
